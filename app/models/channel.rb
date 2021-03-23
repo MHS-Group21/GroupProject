1de29c8acf28bd91a65d4f8e1207bb757d326288
JamesTrewern <trewern.james@gmail.com>
2021-03-23 12:38:18 -0700

    error fixing

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,13 +2,12 @@ class Channel < ApplicationRecord
     has_many :discussions
     has_many :users, through: :discussions
     resourcify
-  
+
     extend FriendlyId
-    friendly_id :channel, use: [:slugged, :finders]
-  
+    friendly_id :name, use: [:slugged, :finders]
+
     def should_generate_new_friendly_id?
-      channel_changed?
+      name_changed?
     end
-  
+
   end
-  
