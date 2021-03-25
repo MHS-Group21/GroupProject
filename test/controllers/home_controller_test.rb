25e8e3d91222074b73d92cdc0c4933a5509d3499
MHS-Group21 <79668489+MHS-Group21@users.noreply.github.com>
2021-03-25 20:54:51 +0000

    Working tests for admin roles and adjusted admin links

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,17 +2,19 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   # Automatically logs the user in for all the tests
-  setup do
-    sign_in users(:one)
-  end
+  # setup do
+  #   sign_in users(:one)
+  # end
 
 # Tests for volunteer action
   test "should get volunteer page" do
+    sign_in users(:one)
     get volunteer_path
     assert_response :success
   end
   
   test "redirect from volunteer_path if not logged in" do
+    sign_in users(:one)
     # Signs out the user for this test
     sign_out :user
     get volunteer_path
@@ -20,6 +22,7 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "redirect if not volunteer" do
+    sign_in users(:one)
     # Signs out the user for this test
     sign_out :user
     sign_in users(:two)
@@ -28,11 +31,34 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "Redirect to root from home_index_path, if not logged in" do
+    sign_in users(:one)
     # Signs out the user for this test
     sign_out :user
     get home_index_path
     assert_response :redirect
   end
 
+  test "should get admin page" do
+    sign_in users(:three)
+    get admin_path
+    assert_response :success
+  end
+
+  test "redirect from admin_path if not logged in" do
+    sign_in users(:three)
+    # Signs out the user for this test
+    sign_out :user
+    get admin_path
+    assert_response :redirect
+  end
+
+  test "redirect if not admin" do
+    sign_in users(:three)
+    # Signs out the user for this test
+    sign_out :user
+    sign_in users(:two)
+    get admin_path
+    assert_response :redirect
+  end
 
 end
