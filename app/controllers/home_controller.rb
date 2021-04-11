b81e5425b1743bd11b26e7512d2d4467c9f4619d
MHS-Group21 <mhs.group21@gmail.com>
2021-04-11 12:28:48 +0100

    Added titles for all views and made tests for twitter page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -42,8 +42,10 @@ class HomeController < ApplicationController
   end
 
   def twitter
+    # Giving a username variable and an options variable that are used as parameters for the user_timeline method
     username = 'MHS_Group21'
     options = {:count => 20, :include_rts => true}
+    # Making a tweets variable that stores the JSON file that is received for user_timeline method
     @tweets = $client.user_timeline(username, options)
     
     # Method to get tweets from home timeline
