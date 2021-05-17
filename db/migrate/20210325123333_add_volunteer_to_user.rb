335b98cd5f8118c64914d82b748508c302d72630
JamesTrewern <trewern.james@gmail.com>
2021-05-17 16:24:02 -0700

    fixed migration error

--- db/migrate/20210325123333_add_volunteer_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddVolunteerToUser < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :volunteer, :Boolean, default: false
-  end
-end
--- db/migrate/20210515151119_delete_volunteer_bool.rb
+++
@@ -1,5 +0,0 @@
-class DeleteVolunteerBool < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :volunteer
-  end
-end
